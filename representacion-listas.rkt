d0101db9d01f4eb29958dd699f73b75b1c5984eb
Bakudi <107456253+Bakudi@users.noreply.github.com>
2024-09-21 10:20:18 -0500

    chips y comentarios

--- representacion-listas.rkt
+++ representacion-listas.rkt
@@ -12,15 +12,16 @@ simple-circuit(in out chip)
                          output {cable} *
                 complex-circuit (circ lcircs in out)
 
-<chip> := <chip p rim>
-prim−chip ( chip−prim )
+<chip> 
+        := <chip-prim>
+        prim−chip ( chip−prim )
         
-        := chip ( −−> {(port)}∗)
+        := comp-chip ( −−> {(port)}∗)
                 ( <−− {(port)}∗)
                 <circuito>
 comp−chip (in,out,circ)                    
 
-<chip prim> := prim o r
+<chip prim> := prim or
             chip−or ( )
     := prim and
     chip−and ( )
@@ -39,5 +40,53 @@ comp−chip (in,out,circ)
 
     := prim xnor
     chip−xnor ( )
-
 |#
+
+;;constructores
+
+(define-type cable symbol)
+(define-type port symbol)
+
+(define (chip-or)
+    (lambda (symbol)
+  (list 'chip-or symbol)))
+
+(define (chip-and)
+    (lambda (symbol)
+  (list 'chip-and symbol)))
+
+(define (chip-not)
+    (lambda (symbol)
+  (list 'chip-not symbol)))
+
+(define (chip-xor)
+    (lambda (symbol)
+  (list 'chip-xor symbol)))
+
+(define (chip-nand)
+    (lambda (symbol)
+  (list 'chip-nand symbol)))
+
+(define (chip-nor)
+    (lambda (symbol)
+  (list 'chip-nor symbol)))
+
+(define (chip-xnor)
+    (lambda (symbol)
+  (list 'chip-xnor symbol)))
+
+(define (prim-chip)
+  (lambda (chip-prim symbol)
+    ((chip-prim) symbol)
+  ))
+
+(define (comp-chip)
+    (lambda (in out circ)
+        (list 'comp-chip in out circ)
+    ))
+
+(define circ_simple
+    (lambda ()
+    
+    
+    ))
